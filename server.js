c2a576c2cbfcf7c45cec93efc2751cdd9154afed
dhzm <dhzm.chile@msn.com>
2025-06-06 09:37:44 -0400

    Testeo numero 2

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Servidor Node.js funcionando\n');
-});
-
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+//otro comentario en java scrpt 
